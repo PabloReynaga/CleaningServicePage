c4e374affffd6fee4a1ca1c9654345c1c9b50e99
PabloReynaga <reynagafarias@gmail.com>
2023-09-13 17:26:57 +0200

    clean unneeded documents // NavBar styling

---
+++ src/components/NavBar.vue
@@ -0,0 +1,39 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+    <div class="navbar-container">
+        
+        <div class="main-logo-container">
+        
+        </div>
+        
+        <div class="nav-items-container">
+
+        </div>
+
+        
+
+    </div>
+    <div class="divider"></div>
+    
+    </div>
+</template>
+
+<style scoped>
+    .navbar-container{
+        display: block;
+        width: 100% ;
+        height: 85px;
+        padding: none;
+        
+    }
+    .divider{
+        border: solid 1px gray;
+    }
+
+
+
+</style>
