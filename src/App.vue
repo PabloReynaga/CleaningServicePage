65884d58b56dff29e100bba815a65ff5c9e6545c
PabloReynaga <reynagafarias@gmail.com>
2023-09-18 22:38:09 +0200

    slide-photos

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import NavBar from './components/NavBar.vue';
 import "../src/assets/main.scss"
-import Home from './components/Home.vue';
+import Home from '././components/section/Home.vue';
 
 
 </script>
