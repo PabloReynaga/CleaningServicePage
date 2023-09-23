c718940b9b94818dcf78e93507b60b524b232420
PabloReynaga <reynagafarias@gmail.com>
2023-09-23 17:15:07 +0200

    slider effect

--- src/components/slider/CarouselItem.vue
+++ src/components/slider/CarouselItem.vue
@@ -6,25 +6,20 @@ const props = defineProps(['slide', 'currentSlide', 'index']);
 
 </script>
 <template>
-    <div>
-        <div class="carousel-item">
-                <div class="img-container" 
-                v-show="currentSlide === index">
-
+    <transition name="slide-in"> 
+                <div class="img-container" v-show="currentSlide === index">
                     <img class="img" :src="slide" >
-                    
                 </div>
-        </div>
-
-    </div>
+    </transition>
 </template>
 
 <style scoped lang="scss">
 .img-container{
-    display: block;
-    height: 800px;
-    width: 100%;
-    z-index: 1;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
 }
 
 .img{
@@ -34,5 +29,15 @@ const props = defineProps(['slide', 'currentSlide', 'index']);
     object-position: center;
 
 
-} 
+}
+.slide-in-enter-active, 
+.slide-in-leave-active{
+    transition: all 1s ease-in-out;
+}
+.slide-in-enter-from{
+    transform: translateX(-100%);
+}
+.slide-in-leave-to{
+    transform: translateX(100%);
+}
 </style>
